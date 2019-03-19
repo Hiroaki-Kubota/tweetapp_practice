1978e145e27f245fe692f2a1ca9330f21caa4441
Hiroaki Kubota <hkubota502@gmail.com>
2019-03-19 11:49:12 +0900

    add post show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,13 @@
 # Controller for Posts
 class PostsController < ApplicationController
   attr_accessor :posts
+  attr_accessor :post
 
   def index
     self.posts = Post.all
+  end
+
+  def show
+    self.post = Post.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/' => 'home#top'
   get 'about' => 'home#about'
   get 'posts' => 'posts#index'
+  get 'posts/:id', to: 'posts#show', as: 'post'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
