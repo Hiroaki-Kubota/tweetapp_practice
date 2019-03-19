231318db52ee02bd3a0fb10eb5a5f5ba3fee88ec
Hiroaki Kubota <hkubota502@gmail.com>
2019-03-19 14:45:35 +0900

    bootstrap追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@
 
 # Controller for Posts
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
   attr_accessor :posts
   attr_accessor :post
 
@@ -9,7 +10,56 @@ class PostsController < ApplicationController
     self.posts = Post.all
   end
 
-  def show
+  def show; end
+
+  def new
+    self.post = Post.new
+  end
+
+  def create
+    self.post = Post.new(post_params)
+    respond_to do |format|
+      if post.save
+        format.html { redirect_to :posts, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: post }
+      else
+        format.html { render :new }
+        format.json { render json: post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if post.update(post_params)
+        format.html { redirect_to :posts, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: post }
+      else
+        format.html { render :edit }
+        format.json { render json: post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1
+  # DELETE /posts/1.json
+  def destroy
+    post.destroy
+    respond_to do |format|
+      format.html { redirect_to :posts, notice: 'Blog was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_post
     self.post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
